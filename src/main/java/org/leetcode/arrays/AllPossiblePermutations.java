c141b13facd776dbbf99a27804edb076d726d516
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-07-24 12:18:58 -0400

    Add AllPossiblePermutations.java: Implements algorithm to generate all permutations of an array

---
+++ src/main/java/org/leetcode/arrays/AllPossiblePermutations.java
@@ -0,0 +1,36 @@
+package org.leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPossiblePermutations {
+
+    public static List<List<Integer>> findAllPossiblePermutations(int nums[]){
+        List<List<Integer>> permutations = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        backtrack(nums, tempList, permutations);
+        return permutations;
+    }
+
+    private static void backtrack(int nums[], List<Integer> tempList, List<List<Integer>> permutations){
+        if(tempList.size() == nums.length){
+            permutations.add(new ArrayList<>(tempList));
+        }else {
+            for(int i=0; i<nums.length; i++) {
+                if(tempList.contains(nums[i])) {
+                    continue;
+                }
+                else {
+                    tempList.add(nums[i]);
+                    backtrack(nums, tempList, permutations);
+                    tempList.remove(tempList.size() - 1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2};
+        System.out.println(findAllPossiblePermutations(arr));
+    }
+}
