5af78099685f55f3bf2ac740643c2952d2fd4c5b
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-07-24 12:20:55 -0400

    Add RemoveDuplicate.java: Implements logic to remove duplicates from an array

---
+++ src/main/java/org/leetcode/arrays/RemoveDuplicate.java
@@ -0,0 +1,25 @@
+package org.leetcode.arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicate {
+    public static int[] removeDuplicate(int []arr){
+        Set<Integer> set = new HashSet<>();
+        for(int item : arr){
+            set.add(item);
+        }
+        int result[] = new int[set.size()];
+        int i = 0;
+        for(int item : set){
+            result[i++] = item;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] inputArray = {1, 2, 3, 2, 4, 5, 1, 6, 5};
+        System.out.println(Arrays.toString(removeDuplicate(inputArray)));
+    }
+}
