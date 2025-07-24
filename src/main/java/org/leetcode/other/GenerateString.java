9049b175323e68995d44088d28f052417d5f661e
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-07-24 12:27:20 -0400

    Add other directory: Contains miscellaneous solutions (ContainsDuplicate, DuplicateZeros, GenerateString, LongestCommonPrefix, TwoSum)

---
+++ src/main/java/org/leetcode/other/GenerateString.java
@@ -0,0 +1,26 @@
+package org.leetcode.other;
+
+import java.util.Random;
+
+public class GenerateString {
+    public static void main(String[] args) {
+        System.out.println(generateString());
+    }
+
+    public static String generateString() {
+
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        return generatedString;
+    }
+}
---
+++ src/main/java/org/leetcode/other/LongestCommonPrefix.java
@@ -0,0 +1,5 @@
+package org.leetcode.other;
+
+public class LongestCommonPrefix {
+
+}
